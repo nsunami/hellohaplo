5b549af4e070b989a9d5c1e9660053761025e8ef
Nami Sunami <naoyuki.sunami@gmail.com>
2023-03-31 21:36:45 +0200

    Delete add_metadata.R

--- R/add_metadata.R
+++
@@ -1,8 +0,0 @@
-# A helper function to get the matadata based on refs
-add_metadata <- function(df) {
-  df |>
-    mutate(metadata = map(refs, function(ref) {
-      get_object_metadata(ref)
-    })) |>
-    unnest(metadata)
-}
