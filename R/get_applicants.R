41ba9a0652f35c12855e0fc1bbfa7526077f63de
Nami Sunami <naoyuki.sunami@gmail.com>
2023-04-01 15:25:18 +0200

    start function documentations for all

--- R/get_applicants.R
+++ R/get_applicants.R
@@ -1,9 +1,20 @@
+#' Get the vector of applicants for the given ethics application
+#'
+#' @param application_ref A object ref
+#'
+#' @return A character vector of refs
+#' @export 
+#'
+#' @examples
+#' application <- get_applicants("8435x")
 get_applicants <- function(application_ref) {
   application_info <- application_ref |>
     get_object_info(sources = "std:username") |>
     httr::content("text") |>
     jsonlite::fromJSON()
-
+  
   # Vector of applicants
-  application_info[["object"]][["attributes"]][["hres:attribute:researcher"]][["ref"]]
+  as.vector(
+    application_info[["object"]][["attributes"]][["hres:attribute:researcher"]][["ref"]]
+  )
 }
--- R/get_people_by_faculty.R
+++ R/get_people_by_faculty.R
@@ -1,4 +1,16 @@
-get_people_by_faculty <- function(faculty_ref, sources = "NONE", query = NULL, key) {
+#' Get people associated with a faculty 
+#'
+#' @param faculty_ref A ref for a faculty object
+#' @param sources Sources as per API
+#' @param key Haplo API Key
+#'
+#' @return A character vector of refs
+#' @export
+#'
+#' @examples
+#' get_people_by_faculty("801yv")
+get_people_by_faculty <- function(faculty_ref, sources = "NONE",
+                                  key = Sys.getenv("HAPLO_API_KEY")) {
   query_api <- function(facutly_ref, start = 0, key) {
     httr::GET(paste0("https://ethicsmonitor.eur.nl/api/v0-object/linked/", faculty_ref),
       httr::authenticate("haplo", key),
